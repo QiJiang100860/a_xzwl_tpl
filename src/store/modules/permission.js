ca94667742faa688230b3e40f03c4b0dff895dcb
jiangqi <18710609128@163.com>
2018-05-18 18:16:29 +0800

    权限控制过滤逻辑添加

--- src/store/modules/permission.js
+++ src/store/modules/permission.js
@@ -5,32 +5,38 @@ import { asyncRouterMap, constantRouterMap } from '@/router'
  * @param roles
  * @param route
  */
-function hasPermission(roles, route) {
-  if (route.meta && route.meta.roles) {
-    return roles.some(role => route.meta.roles.indexOf(role) >= 0)
-  } else {
-    return true
-  }
-}
+// function hasPermission(roles, route) {
+//   if (route.meta && route.meta.roles) {
+//     return roles.some(role => route.meta.roles.indexOf(role) >= 0)
+//   } else {
+//     return true
+//   }
+// }
 
 /**
  * 递归过滤异步路由表，返回符合用户角色权限的路由表
  * @param asyncRouterMap
  * @param roles
  */
-function filterAsyncRouter(asyncRouterMap, roles) {
+// function filterAsyncRouter(asyncRouterMap, roles) {
+//   const accessedRouters = asyncRouterMap.filter(route => {
+//     if (hasPermission(roles, route)) {
+//       if (route.children && route.children.length) {
+//         route.children = filterAsyncRouter(route.children, roles)
+//       }
+//       return true
+//     }
+//     return false
+//   })
+//   return accessedRouters
+// }
+function filterAsyncRouter1(asyncRouterMap, permissionTree) {
+  debugger
   const accessedRouters = asyncRouterMap.filter(route => {
-    if (hasPermission(roles, route)) {
-      if (route.children && route.children.length) {
-        route.children = filterAsyncRouter(route.children, roles)
-      }
-      return true
-    }
-    return false
+    return route.path.indexOf(permissionTree[0].path) > 0
   })
   return accessedRouters
 }
-
 const permission = {
   state: {
     routers: constantRouterMap,
@@ -46,25 +52,29 @@ const permission = {
     GenerateRoutes({ commit }, data) {
       return new Promise(resolve => {
         const { roles, permissionTree } = data
-        let accessedRouters
+        // let accessedRouters
         if (roles.indexOf('admin') >= 0) {
-          accessedRouters = asyncRouterMap
+          alert('admin')
         } else {
-          accessedRouters = filterAsyncRouter(asyncRouterMap, roles)
+          alert('editor')
         }
+        let accessedRouters1
         // 根据后台标识过滤路由表
         if (permissionTree === 'all') {
+          accessedRouters1 = asyncRouterMap
           console.log('全部路由表')
         } else {
-          for (const item of permissionTree) {
-            if (item.hasChildren) {
-              for (const citem of item.children) {
-                console.log(citem)
-              }
-            }
-          }
+          accessedRouters1 = filterAsyncRouter1(asyncRouterMap, permissionTree)
+          // for (const item of permissionTree) {
+          //   if (item.hasChildren) {
+          //     for (const citem of item.children) {
+          //       console.log(citem)
+          //     }
+          //   }
+          // }
         }
-        commit('SET_ROUTERS', accessedRouters)
+        // commit('SET_ROUTERS', accessedRouters)
+        commit('SET_ROUTERS', accessedRouters1)
         resolve()
       })
     }
